be2794a2485ba158c7341fcd85b8e225b75c9736
ChristophEnglisch <englisch.ch@gmail.com>
2022-08-15 20:16:07 +0200

    Blog Page

--- app/src/components/Blog.vue
+++ app/src/components/Blog.vue
@@ -1,12 +1,14 @@
 <template>
-$END$
+  <v-container id="blog">
+
+  </v-container>
 </template>
 
 <script lang="ts">
 import {Component, Vue} from 'vue-property-decorator'
 
 @Component()
-export default class Blog extends Vue{
-    
+export default class Blog extends Vue {
+
 }
 </script>
