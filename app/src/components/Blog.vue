dc2beefd04443b65c60df12a01cb0428c57ebec8
ChristophEnglisch <englisch.ch@gmail.com>
2022-08-15 20:10:10 +0200

    New Pages

---
+++ app/src/components/Blog.vue
@@ -0,0 +1,12 @@
+<template>
+$END$
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator'
+
+@Component()
+export default class Blog extends Vue{
+    
+}
+</script>
---
+++ app/src/components/Contact.vue
@@ -0,0 +1,12 @@
+<template>
+$END$
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator'
+
+@Component()
+export default class Contact extends Vue{
+    
+}
+</script>
